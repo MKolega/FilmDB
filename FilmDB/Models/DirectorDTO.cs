98c35bcd22b684c78aef5d04f6b16f3a94716a06
MKolega <mkolega@tvz.hr>
2024-06-14 01:21:03 +0200

    Added API functionality

---
+++ FilmDB/Models/DirectorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace FilmDB.Web.Models
+{
+	public class DirectorDTO
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		
+	}
+}
---
+++ FilmDB/Models/MoviesDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace FilmDB.Web.Models
+{
+	public class MoviesDTO
+	{
+		public int ID { get; set; }
+		public string Title { get; set; }
+		public string? Description { get; set; }
+	
+		public string? Image { get; set; }
+	
+		public string? Genre { get; set; }
+
+		public int Year { get; set; }
+		public DirectorDTO? Director { get; set; }
+		
+	}
+}
